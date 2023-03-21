9fa03b05094f68d8cdc8a2d74637d9f56c07a147
David Sanders <dsanders_m633@outlook.com>
2023-03-21 16:38:15 -0400

    update

--- src/_SevenCorners/scenes/Hero/Export/Comp.tsx
+++ src/_SevenCorners/scenes/Hero/Export/Comp.tsx
@@ -8,7 +8,7 @@ export default function Comp({ }: IExport.Props) {
     return (
         <>
             <Row className={`${styles.heroSize} d-flex justify-content-center sc-bg-typography`}>
-                <Col sm={12} md={8} className={`d-flex align-items-center py-5`}>
+                <Col sm={12} md={8} className={`d-flex align-items-center p-5 px-md-0`}>
                     <div>
                         <h1 className={"text-center text-md-start"}>This is a Hero Banner</h1>
                         <p className={'text-center text-md-start fs-3'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
