aad21f51ed7b3964c4ca62efafbba8763271a13c
David Sanders <dsanders_m633@outlook.com>
2023-03-24 10:08:07 -0400

    fixed hero overflow issues

--- src/_SevenCorners/scenes/Hero/Export/Comp.tsx
+++ src/_SevenCorners/scenes/Hero/Export/Comp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import { IExport } from './interfaces';
 import styles from './HeroBanner.module.scss';
 
@@ -7,14 +7,18 @@ export default function Comp({ }: IExport.Props) {
 
     return (
         <>
-            <Row className={`${styles.heroSize} d-flex justify-content-center sc-bg-stripes`}>
-                <Col sm={12} md={8} className={`d-flex align-items-center p-5 px-md-0`}>
-                    <div>
-                        <h1 className={"text-center text-md-start"}>This is a Hero Banner</h1>
-                        <p className={'text-center text-md-start fs-3'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    </div>
-                </Col>
-            </Row>
+            <div className={`${styles.heroSize} sc-bg-stripes`}>
+                <Container>
+                    <Row>
+                        <Col sm={12} className={`d-flex align-items-center p-5 px-md-0`}>
+                            <div>
+                                <h1 className={"text-center text-md-start"}>This is a Hero Banner</h1>
+                                <p className={'text-center text-md-start fs-3'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>  
         </>
     )
 }
